5a53bec2f4e8d9260a48b59a0456b20a9af6b726
zji <yitozji@163.com>
2019-10-22 20:16:35 +0800

    1、完善注册页面 2、开发环境启动打开浏览器

--- src/views/sign.vue
+++ src/views/sign.vue
@@ -1,36 +1,35 @@
 <template>
-  <v-form v-model="valid">
+  <v-form ref="form" v-model="valid">
     <v-container>
       <v-row>
         <v-col
           cols="12"
-          md="4"
+          md="12"
         >
           <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
+            v-model="username"
+            :counter="32"
+            :rules="usernameRules"
+            label="用户名"
             required
           ></v-text-field>
         </v-col>
 
         <v-col
           cols="12"
-          md="4"
+          md="12"
         >
           <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
+            v-model="password"
+            label="密码"
+            :rules="passwordRules"
             required
           ></v-text-field>
         </v-col>
 
         <v-col
           cols="12"
-          md="4"
+          md="12"
         >
           <v-text-field
             v-model="email"
@@ -38,6 +37,13 @@
             label="E-mail"
             required
           ></v-text-field>
+        </v-col>
+        <v-col
+          cols="12"
+          md="12"
+          class="text-center"
+        >
+          <v-btn large color="primary" @click="sign">注册</v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -48,7 +54,26 @@
   export default {
     name: 'sign',
     data() {
-      return {}
+      return {
+        usernameRules: [
+          v => !!v || '不能为空',
+          v => (v && v.length <= 32) || '不能超过32字符',
+        ],
+        passwordRules: [
+          v => !!v || '不能为空',
+        ],
+        emailRules: [
+          v => !!v || '不能为空',
+          v => /.+@.+\..+/.test(v) || '格式非法',
+        ],
+      }
+    },
+    methods: {
+      sign() {
+        if (this.$refs.form.validate()) {
+          alert("调后台")
+        }
+      }
     },
     created() {
     },
